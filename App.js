c591a8f94ce83e19870a464563e03adfe6ec9397
salouni <salouniraghuwanshi@gmail.com>
2020-12-17 18:21:56 +0530

    updare file

--- App.js
+++ App.js
@@ -1,186 +1,44 @@
 import React, { useState } from "react";
 import "./styles.css";
-const AllFood = {
-  "🍕": "Pizza",
-  "🍔": " Hamburger",
-  "🧁": " Cupcake",
-  "🍨": "Ice Cream",
-  "🥪": "Sandwich",
-  "🍖": "Meat",
-  "🍗": " Poultry Leg",
-  "🧇": "Waffle",
-  "🥩": "Cut of Meat"
-};
-
-var food = Object.keys(AllFood);
-
+import { foodInfo } from "./list";
 export default function App() {
-  const [meaning, setmeaning] = useState("");
-  function inputHandler(event) {
-    var userInput = event.target.value;
-    var meaning = AllFood[userInput];
-    if (meaning === undefined) {
-      var meaning = "We do not have this in our database";
-    }
-    setmeaning(meaning);
-  }
-  function emojiclick(emoji) {
-    var meaning = AllFood[emoji];
-    setmeaning(meaning);
+  const FoodList = ["All Food", "Veg Food", "Non Veg Food"];
+
+  console.log("FoodList", FoodList);
+  const [foodtype, setfoodtype] = useState("All Food");
+
+  function foodHandler(type) {
+    setfoodtype(type);
   }
   return (
     <div className="App">
-      <h1>input outt!</h1>
-      <input onChange={inputHandler}></input>
-      <h2>{meaning}</h2>
-      <ul>
-        <li
-          style={{
-            display: "inline",
-            padding: "1rem",
-            margin: "auto"
-          }}
-        >
-          <a
-            style={{
-              textDecoration: "none",
-              padding: "2rem",
-              fontWeight: "bold"
-            }}
-            href="/"
-          >
-            <span style={{ border: "1px solid", padding: "0.5rem" }}>
-              AllFood
-            </span>
-          </a>
-          <a href="https://qvh4e.csb.app" style={{ textDecoration: "none" }}>
-            <span
-              style={{
-                border: "1px solid",
-                padding: "0.5rem"
-              }}
-            >
-              VegFood
-            </span>
-          </a>
-          <a
-            href="https://f13fp.csb.app"
-            style={{
-              textDecoration: "none",
-              padding: "2rem"
-            }}
-          >
-            <span style={{ border: "1px solid", padding: "0.5rem" }}>
-              NonVegFood
-            </span>
-          </a>
-        </li>
-      </ul>
-      <h3>We Know</h3>
-      {food.map(function (emoji) {
-        return (
-          <span
-            onClick={() => emojiclick(emoji)}
-            key={emoji}
-            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
-          >
-            {emoji}
-          </span>
-        );
-      })}
-    </div>
-  );
-}
-import React, { useState } from "react";
-import "./styles.css";
-const AllFood = {
-  "🍕": "Pizza",
-  "🍔": " Hamburger",
-  "🧁": " Cupcake",
-  "🍨": "Ice Cream",
-  "🥪": "Sandwich",
-  "🍖": "Meat",
-  "🍗": " Poultry Leg",
-  "🧇": "Waffle",
-  "🥩": "Cut of Meat"
-};
+      <h1>Food</h1>
+      <h2>Tasty Food!!</h2>
+      <hr />
+      <div className="FoodList">
+        {FoodList.map((type) => {
+          return (
+            <button key={type} value={type} onClick={() => foodHandler(type)}>
+              {type}
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        {foodInfo[foodtype].map((food) => {
+          return (
+            <div className="food">
+              <div className="food-info">
+                <h1>
+                  <em>{food.name}</em>{" "}
+                </h1>
 
-var food = Object.keys(AllFood);
-
-export default function App() {
-  const [meaning, setmeaning] = useState("");
-  function inputHandler(event) {
-    var userInput = event.target.value;
-    var meaning = AllFood[userInput];
-    if (meaning === undefined) {
-      var meaning = "We do not have this in our database";
-    }
-    setmeaning(meaning);
-  }
-  function emojiclick(emoji) {
-    var meaning = AllFood[emoji];
-    setmeaning(meaning);
-  }
-  return (
-    <div className="App">
-      <h1>input outt!</h1>
-      <input onChange={inputHandler}></input>
-      <h2>{meaning}</h2>
-      <ul>
-        <li
-          style={{
-            display: "inline",
-            padding: "1rem",
-            margin: "auto"
-          }}
-        >
-          <a
-            style={{
-              textDecoration: "none",
-              padding: "2rem",
-              fontWeight: "bold"
-            }}
-            href="/"
-          >
-            <span style={{ border: "1px solid", padding: "0.5rem" }}>
-              AllFood
-            </span>
-          </a>
-          <a href="https://qvh4e.csb.app" style={{ textDecoration: "none" }}>
-            <span
-              style={{
-                border: "1px solid",
-                padding: "0.5rem"
-              }}
-            >
-              VegFood
-            </span>
-          </a>
-          <a
-            href="https://f13fp.csb.app"
-            style={{
-              textDecoration: "none",
-              padding: "2rem"
-            }}
-          >
-            <span style={{ border: "1px solid", padding: "0.5rem" }}>
-              NonVegFood
-            </span>
-          </a>
-        </li>
-      </ul>
-      <h3>We Know</h3>
-      {food.map(function (emoji) {
-        return (
-          <span
-            onClick={() => emojiclick(emoji)}
-            key={emoji}
-            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
-          >
-            {emoji}
-          </span>
-        );
-      })}
+                <h3>Rating : {food.Rating}</h3>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ list.js
@@ -0,0 +1,73 @@
+export const foodInfo = {
+    "All Food": [
+      {
+        name: "Pizaa",
+        Rating: "5/5"
+      },
+      {
+        name: "Sandwhich  ",
+        Rating: " 4.3/5"
+      },
+      {
+        name: "HumBurger",
+        Rating: "4.7/5"
+      },
+      {
+        name: "Samosa",
+        Rating: " 3.9/5"
+      },
+      {
+        name: "Poultry Leg",
+        Rating: " 3.8/5"
+      },
+      {
+        name: "Waffle",
+        Rating: " 4.2/5"
+      },
+      {
+        name: "Cut of Meat",
+        Rating: " 4.7/5"
+      },
+      {
+        name: "meat",
+        Rating: " 3.9/5"
+      }
+    ],
+    "Veg Food": [
+      {
+        name: "Pizza",
+        Rating: " 5/5"
+      },
+      {
+        name: "Sandwhich  ",
+        Rating: " 4.3/5"
+      },
+      {
+        name: "Humberger",
+        Rating: " 4.7/5"
+      },
+      {
+        name: "Samosa",
+        Rating: " 3.9/5"
+      }
+    ],
+    "Non Veg Food": [
+      {
+        name: "meat",
+        Rating: " 3.8/5"
+      },
+      {
+        name: "cut of meat",
+        Rating: " 4.2/5"
+      },
+      {
+        name: "Waffle",
+        Rating: " 4.7/5"
+      },
+      {
+        name: "Poultry Leg",
+        Rating: " 3.9/5"
+      }
+    ]
+  };
+  
